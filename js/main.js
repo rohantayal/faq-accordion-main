86492c09bd7accc5cd634a4e651aa1f951f7c5c6
Rohan Tayal <109153434+rohantayal@users.noreply.github.com>
2024-05-02 13:16:52 +0530

    Added DOMContent load event listener
    Added DOMContent load event listener

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
-const accordians = document.querySelectorAll('.qs');
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const accordians = document.querySelectorAll('.qs');
 
+    console.log(accordians);
+    console.log(accordians);
+    accordians.forEach(qs => {
+        const icon = qs.querySelector('.icon');
+        const answer = qs.querySelector('.answer');
 
-console.log(accordians);
-accordians.forEach(qs => {
-    const icon = qs.querySelector('.icon');
-    const answer = qs.querySelector('.answer');
+        icon.addEventListener('click', () => {
 
-    icon.addEventListener('click', () => {
+            if (icon.classList.contains('active')) {
+                icon.classList.remove('active');
+                answer.classList.remove('active');
+                icon.alt = "Expand";
 
-        if (icon.classList.contains('active')) {
-            icon.classList.remove('active');
-            answer.classList.remove('active');
-           icon.alt="Expand";
-
-        }
-        else {
-            icon.classList.add('active');
-            answer.classList.add('active');
-            icon.alt="Collapse";
-        }
-    })
+            }
+            else {
+                icon.classList.add('active');
+                answer.classList.add('active');
+                icon.alt = "Collapse";
+            }
+        })
+    });
 })
+
+
