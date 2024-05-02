9f9c6c2f0eadf250c8af60979fa4c01247b9c8d1
Rohan Tayal <109153434+rohantayal@users.noreply.github.com>
2024-05-02 13:06:55 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
+const accordians = document.querySelectorAll('.qs');
 
+
+console.log(accordians);
+accordians.forEach(qs => {
+    const icon = qs.querySelector('.icon');
+    const answer = qs.querySelector('.answer');
+
+    icon.addEventListener('click', () => {
+
+        if (icon.classList.contains('active')) {
+            icon.classList.remove('active');
+            answer.classList.remove('active');
+           icon.alt="Expand";
+
+        }
+        else {
+            icon.classList.add('active');
+            answer.classList.add('active');
+            icon.alt="Collapse";
+        }
+    })
+})
